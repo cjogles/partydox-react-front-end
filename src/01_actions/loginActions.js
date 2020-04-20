8bd4786f1729e14c1bb10b954aa8393d7cd171ea
cjogles <jackson.ogles28@gmail.com>
2020-04-19 18:08:53 -0600

    tidied up login components and redux flow for login

---
+++ src/01_actions/loginActions.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+
+export const login = ( credentials, props ) => dispatch => {
+    dispatch({ 
+      type: LOGIN_START,
+      payload: "Loading Your Partydox Dashboard..."
+    });
+    axios
+      .post('https://partydox.herokuapp.com/friends/login', credentials)
+        .then(res => {
+          dispatch({ type: LOGIN_SUCCESS, payload: res.data })
+          props.history.push('/dashboard')
+          })
+        .catch(err => {
+          dispatch({ type: LOGIN_FAIL, payload: err })
+          });
+  };
