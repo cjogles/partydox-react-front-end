119dfc3163cbdac6d7ddbcf134cce9d408fcd9fa
cjogles <jackson.ogles28@gmail.com>
2020-05-06 11:57:14 -0600

    added links on details components

--- src/components/activities/ActivityDetails.js
+++ src/components/activities/ActivityDetails.js
@@ -21,6 +21,12 @@ function ActivityDetails(props) {
           <p>Activity Likes: {activity.activityLikes}</p>
           <p>Activity Notes: {activity.activityNotes}</p>
         </div>
+        <div className="tripstuff2">
+          <Link to="/activities">Trip Activities</Link>
+          <Link to="/parkingLots">Trip Parking Lots</Link>
+          <Link to="/shoppingLists">Trip Shopping Lists</Link>
+          <Link to="/flights">Trip Flights</Link>
+        </div>
       </div>
     </>
   );
--- src/components/flights/FlightDetails.js
+++ src/components/flights/FlightDetails.js
@@ -40,7 +40,12 @@ function FlightDetails(props) {
           <p>TOTAL COST: {flight.flightCost}</p>
           <p>Likes: {flight.flightLikes}</p>
         </div>
-
+        <div className="tripstuff2">
+          <Link to="/activities">Trip Activities</Link>
+          <Link to="/parkingLots">Trip Parking Lots</Link>
+          <Link to="/shoppingLists">Trip Shopping Lists</Link>
+          <Link to="/flights">Trip Flights</Link>
+        </div>
       </div>
     </>
   );
