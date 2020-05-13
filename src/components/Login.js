bc1892840c3107ff6924b9accd8ddd780a69749d
cjogles <jackson.ogles28@gmail.com>
2020-05-13 08:27:16 -0600

    editing login component to use react hook form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Nav from "./Nav";
 import FooterSignUp from "./FooterSignUp";
+import { useForm } from "react-hook-form";
 import { connect } from "react-redux";
 import { login } from "../actions/signUpActions";
 import { Redirect, useHistory } from "react-router-dom";
 
 function Login(props) {
-
-  let history = useHistory();
-  const [user, setUser] = useState({ username: "", password: "" });
-
-  const onChange = (event) => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    props.login(user, history);
-  };
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
 
   return (
     <>
-      {props.loggedIn && <Redirect push to="/dashboard" />}
       <Nav />
       <div className="loginWrapper">
         <div className="login">
@@ -29,27 +19,23 @@ function Login(props) {
             <span role="img" aria-label="partyface">
               🥳
             </span>
-            Login Here
+            Sign Up Here
           </div>
-
-          <form onSubmit={(event) => onSubmit(event)}>
-            <label htmlFor="username">Username:</label>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input name="username" ref={register({ required: true })} />
+            {/* Password expression. Password must be between 4 and 8 digits long and include at least one numeric digit. */}
             <input
-              type="text"
-              id="username"
-              name="username"
-              value={user.username}
-              onChange={(event) => onChange(event)}
-            ></input>
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
               name="password"
-              value={user.password}
-              onChange={(event) => onChange(event)}
-            ></input>
-            <button>Submit</button>
+              ref={register({ required: true, pattern: /^(?=.*\d).{8,20}$/ })}
+            />
+            {errors.username && <span>Username is required</span>}
+            {errors.password && (
+              <span>
+                Password is required, needs to be between 8 and 20 characters,
+                and must contain a number{" "}
+              </span>
+            )}
+            <input type="submit" />
           </form>
         </div>
       </div>
